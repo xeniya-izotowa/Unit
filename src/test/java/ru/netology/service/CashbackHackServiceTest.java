e71f46252a1bb18eff5270451dd5cc758b9ce2c8
xeniya <xeniia.izotowa@gmail.com>
2021-11-06 15:59:59 +0300

    Tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,27 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemain1IfAmountIs999() {
-        assertEquals(service.remain(999), 1);
+        assertEquals(1, service.remain(999));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemain0IfAmountIs1000() {
-        assertEquals(service.remain(1000), 0);
+        assertEquals(0, service.remain(1000));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemain999IfAmountIs1001() {
-        assertEquals(service.remain(1001), 999);
+        assertEquals(999, service.remain(1001));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemain1000IfAmountIs0() {
-        assertEquals(service.remain(0), 1000);
+        assertEquals(1000, service.remain(0));
     }
 }
