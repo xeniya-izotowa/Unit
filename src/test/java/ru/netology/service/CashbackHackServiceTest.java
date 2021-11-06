e4dcce5b857a7df7f7eb7570c88b4d7d3c5fd6ed
xeniya <xeniia.izotowa@gmail.com>
2021-11-06 16:28:37 +0300

    Tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,26 +1,27 @@
 package ru.netology.service;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.junit.Test
+    @Test
     public void shouldRemain1IfAmountIs999() {
         assertEquals(1, service.remain(999));
     }
 
-    @org.junit.Test
+    @Test
     public void shouldRemain0IfAmountIs1000() {
         assertEquals(0, service.remain(1000));
     }
 
-    @org.junit.Test
+    @Test
     public void shouldRemain999IfAmountIs1001() {
         assertEquals(999, service.remain(1001));
     }
 
-    @org.junit.Test
+    @Test
     public void shouldRemain1000IfAmountIs0() {
         assertEquals(1000, service.remain(0));
     }
